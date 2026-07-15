909cfbd5e1f293aae2512e4eb5d17ed8d56d2c63
Nisarga <nisargakshetty@gmail.com>
2026-07-15 14:06:38 +0530

    deployment testing

--- backend/src/utils/asyncHandler.js
+++ backend/src/utils/asyncHandler.js
@@ -1,6 +1,24 @@
+// const asyncHandler = (handler) => {
+//   return (req, res, next) => {
+//     Promise.resolve(handler(req, res, next)).catch(next);
+//   };
+// };
+
+// export default asyncHandler;
+
+
+
 const asyncHandler = (handler) => {
-  return (req, res, next) => {
-    Promise.resolve(handler(req, res, next)).catch(next);
+  return async (req, res, next) => {
+    try {
+      await handler(req, res, next);
+    } catch (err) {
+      console.error("========== ERROR ==========");
+      console.error(err);
+      console.error(err.stack);
+      console.error("===========================");
+      next(err);
+    }
   };
 };
 
